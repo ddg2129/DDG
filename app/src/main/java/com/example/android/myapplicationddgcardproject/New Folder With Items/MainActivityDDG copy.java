b9aa87836bef61622f9cc3b2f2ae62f2389df57a
DDG <darengratts@DDG.local>
2016-09-13 15:26:44 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/myapplicationddgcardproject/New Folder With Items/MainActivityDDG copy.java
@@ -0,0 +1,13 @@
+package com.example.android.myapplicationddgcardproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityDDG extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity_ddg);
+    }
+}
